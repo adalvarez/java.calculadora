f5389db9903196dad8b3c101bd288e76a1d263ab
chapitro <adrianchapi@gmail.com>
2015-07-03 17:09:49 -0600

    Main limpio

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -6,11 +6,7 @@ public class Calculadora {
 
 
     public static void main(String[] args) {
-        
-        metodos ob = new metodos();
-        
-        double o = ob.genericaR(4,3);
-        System.out.println(o);
+       
     }
     
 }
