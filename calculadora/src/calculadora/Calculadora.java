1debbbe30ad9b84e12cb66cf79d5c9e882653ad8
chapitro <adrianchapi@gmail.com>
2015-07-04 23:47:53 -0600

    Interfaz por línea de comandos
    PD: Testing, validaciones especiales como logaritmos negativos, etc.

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -1,12 +1,100 @@
 
 package calculadora;
-
+import java.util.Scanner;
 
 public class Calculadora {
 
-
     public static void main(String[] args) {
-       
+        ejecucion();
+    }
+    
+    public static void ejecucion(){
+        String menu = "[menu]\nBienvenido, Elige una opción:\n\t1. Suma\n\t2. Resta\n\t3. Multiplicación"
+               + "\n\t4. División\n\t5. Módulo\n\t6. Potencias"
+               + "\n\t7. Valor absoluto\n\t8. Redondeo"
+               + "\n\t9. Trigonometría\n\t10. Logaritmos"
+               + "\n\t11. Constantes Especiales"
+               + "\n\t12. Factorial\n\t13. Raices\n\t14.Salir";
+        System.out.println(menu);
+        System.out.print("Elige una opcion > ");
+        Scanner eleccion = new Scanner(System.in);
+        metodos llamada = new metodos();
+        switch (eleccion.nextLine()){
+            case "1": llamada.suma();break;
+            case "2": llamada.resta();break;
+            case "3": llamada.multiplicacion();break;
+            case "4": llamada.division();break;
+            case "5": llamada.modulo();break;
+            case "6": 
+                String potenciaS = "\nElige una opción:\n\t1.Potencia genérica x^y\n\t2.Potencia Cuadrada\n\t3.Potencia Cúbica";
+                System.out.println(potenciaS);
+                System.out.print("Elige una opcion > ");
+                Scanner potenciaEleccion = new Scanner(System.in);
+                switch(potenciaEleccion.nextLine()){
+                    case "1": llamada.potenciaG(); break;
+                    case "2": llamada.potenciaCua(); break;
+                    case "3": llamada.potenciaCub(); break;
+                    default: System.out.println("[error]: El valor ingresado no es correcto");
+                }
+                break;
+            case "7": llamada.valorAbs();break;
+            case "8": llamada.redondear();break;
+            case "9": 
+                String trigoS = "\nElige una opción:\n\t1.Seno\n\t2.Coseno\n\t3.Tangente\n\t4.ArcSeno\n\t5.ArcCoseno\n\t6.ArcTangente";
+                System.out.println(trigoS);
+                System.out.print("Elige una opcion > ");
+                Scanner trigoEleccion = new Scanner(System.in);
+                switch(trigoEleccion.nextLine()){
+                    case "1": llamada.senoT(); break;
+                    case "2": llamada.cosenoT(); break;
+                    case "3": llamada.tangenteT(); break;
+                    case "4": llamada.arcSenoT(); break;
+                    case "5": llamada.arcCosenoT(); break;
+                    case "6": llamada.arcTangenteT(); break;
+                    default: System.out.println("[error]: El valor ingresado no es correcto");
+                }
+                break;
+            case "10": 
+                String logS = "\nElige una opción:\n\t1.Logartimo natural\n\t2.Logaritmo base 10";
+                System.out.println(logS);
+                System.out.print("Elige una opcion > ");
+                Scanner logEleccion = new Scanner(System.in);
+                switch(logEleccion.nextLine()){
+                    case "1": llamada.logaritmoNatural(); break;
+                    case "2": llamada.logaritmoBase10(); break;
+                    default: System.out.println("[error]: El valor ingresado no es correcto");
+                }
+                break;
+            case "11": 
+                String constanteS = "\nElige una opción:\n\t1.Constante E\n\t2.Constante PI";
+                System.out.println(constanteS);
+                System.out.print("Elige una opcion > ");
+                Scanner constanteEleccion = new Scanner(System.in);
+                switch(constanteEleccion.nextLine()){
+                    case "1": llamada.constanteE(); break;
+                    case "2": llamada.constantePI(); break;
+                    default: System.out.println("[error]: El valor ingresado no es correcto");
+                }
+                break;
+            case "12": llamada.factorial();break;
+            case "13": 
+                String raizS = "\nElige una opción:\n\t1.Raiz genérica (x)^1/y\n\t2.Raiz Cuadrada\n\t3.Raiz Cúbica";
+                System.out.println(raizS);
+                System.out.print("Elige una opcion > ");
+                Scanner raizEleccion = new Scanner(System.in);
+                switch(raizEleccion.nextLine()){
+                    case "1": llamada.raizG(); break;
+                    case "2": llamada.raizCua(); break;
+                    case "3": llamada.raizCub(); break;
+                    default: System.out.println("[error]: El valor ingresado no es correcto");
+                }
+                break;
+            case "14":
+                System.exit(0);
+            default: System.out.println("[error]: El valor ingresado no es correcto");
+        }
+        System.out.println("\n[tarea_terminada]\n");
+        ejecucion();
     }
     
 }
--- calculadora/src/calculadora/metodos.java
+++ calculadora/src/calculadora/metodos.java
@@ -1,56 +1,384 @@
 
 package calculadora;
+import java.util.LinkedList;
 
-public class metodos {
+import java.util.Scanner;
+
+public class metodos{
     
-    // Operaciones principales
-        // Uso de int ya que nos permite un máximo de 2 mil millones (positivos y negativos)
-    public int sumaEntero ( int op1, int op2){ return (op1 + op2); }
-    public int restaEntero ( int op1, int op2){ return (op1 - op2); }
-    public int multiEntero ( int op1, int op2){ return (op1 * op2); }
-    public int divisionEntero ( int op1, int op2){ return (op1 / op2); }
-    public int moduloEntero ( int op1, int op2){ return (op1 % op2); }
+    public LinkedList processMain (){
+        LinkedList salida = new LinkedList();
+        
+        int opUno= 0; int opDos = 0;
+        boolean condUnoN = false; boolean condDosN = false;
+        float opUnoF = 0; float opDosF = 0;
+        boolean condUnoF = false; boolean condDosF = false;
+        
+        Scanner operandoUno = new Scanner(System.in);
+        Scanner operandoDos = new Scanner(System.in);
+        try{
+            System.out.print("Ingresa el operando 1 > ");
+            opUno = operandoUno.nextInt();
+            condUnoN = true;
+        }catch(Exception e){
+            try{
+                opUnoF = operandoUno.nextFloat();
+                condUnoF = true;
+            }catch(Exception f){
+                System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero o flotante.\n");
+                processMain();
+            }
+        }
+        try{
+            System.out.print("Ingresa el operando 2 > ");
+            opDos = operandoDos.nextInt();
+            condDosN = true;
+        }catch(Exception e){
+            try{
+                opDosF = operandoDos.nextFloat();
+                condDosF = true;
+            }catch(Exception f){
+                System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero o flotante.\n");
+                processMain();
+            }
+        }
+        salida.add(opUno); salida.add(opDos); salida.add(condUnoN); salida.add(condDosN);
+        salida.add(opUnoF); salida.add(opDosF); salida.add(condUnoF); salida.add(condDosF);
+        return salida;
+    }
     
-    // Operaciones avanzadas
-    public double potencia (double op1, double op2){ return Math.pow(op1, op2);} // x^y
-    public double potenciaCuadrada (double op1){ return Math.pow(op1,2);} // x^2
-    public double potenciaCubica (double op1){ return Math.pow(op1,3);} // x^3
     
-    public double absolutoDoubleEntero ( double op1){ return Math.abs(op1); }  // Valor absoluto de un double
-    public int absolutoIntEntero ( int op1){ return Math.abs(op1); }  // Valor absoluto de un entero
-    
-    public double redondeo ( double op1){ return Math.round(op1);} // Redondeo
-    
-    public double coseno (double op1){ return Math.cos(op1);}
-    public double seno (double op1){ return Math.sin(op1);}
-    public double tangente (double op1){ return Math.tan(op1);}
-    // identidades trigonométricas inversas
-    public double cosenoInv (double op1){ return Math.acos(op1);}
-    public double senoInv (double op1){ return Math.asin(op1);}
-    public double tangenteInv (double op1){ return Math.atan(op1);}
-    
-    public double logNatural (double op1){ return Math.log(op1);} // Logaritmo natural (base e) de op1
-    public double logBase10 (double op1){ return Math.log10(op1);} // Logaritmo (base 10) de op1
-    
-    public double constE (){ return Math.E;}  // Retorna el valor de la constante E
-    public double constPI (){ return Math.PI;}  // Retorna el valor de la constante PI
-    
-    // Funcion factorial recursiva
-    private int tempFactorial = 1;
-    public int factorialRecursivo (int op1){
-        if (op1 == 0 || op1 == 1){ return 1; }
-        else{
-            tempFactorial = op1 * factorialRecursivo(op1 -1 );
-            return tempFactorial;
+    ////////////////////////////////////////////
+    public void suma(){
+        System.out.println("\n[suma]");
+        LinkedList salida = processMain();
+        // Casting de variables que proceden del LinkedList
+        Integer opUno = (Integer)salida.get(0); Integer opDos = (Integer)salida.get(1);
+        Boolean condUnoN = (Boolean)salida.get(2); Boolean condDosN = (Boolean)salida.get(3);
+        Float opUnoF = (Float)salida.get(4); Float opDosF = (Float)salida.get(5);
+        Boolean condUnoF = (Boolean)salida.get(6); Boolean condDosF = (Boolean)salida.get(7);
+        
+        operaciones operacion = new operaciones();
+        
+        if (condUnoN && condDosN){ // ambos son enteros y están listo para realizar la operación
+            System.out.println("El resultado es: " + operacion.sumaEntero(opUno, opDos));
+        }else if(condUnoN && condDosF){
+            float tempOpUno = (float)opUno;
+            System.out.println("El resultado es: " + operacion.sumaFloat(tempOpUno, opDosF));
+        }else if(condUnoF && condDosN){
+            float tempOpDos = (float)opDos;
+            System.out.println("El resultado es: " + operacion.sumaFloat(opUnoF, tempOpDos));
+        }else{ // ambos son flotantes
+            System.out.println("El resultado es: " + operacion.sumaFloat(opUnoF, opDosF));
         }
     }
     
-    // Raices --
-    public double genericaR (double op1, double index)
-    { // Raiz generica para cualesquiera dos números enteros
-        return Math.pow(Math.E, Math.log(op1)/index);
-        //return  Math.pow(op1, 1/index);
-    } 
-    public double cuadradoR (double op1){ return Math.sqrt(op1);} // Raiz cuadrada
-    public double cubicoR (double op1){ return Math.cbrt(op1);} // Raiz cubica
+    ////////////////////////////////////////////
+    public void resta(){
+        System.out.println("\n[resta]");
+        LinkedList salida = processMain();
+        // Casting de variables que proceden del LinkedList
+        Integer opUno = (Integer)salida.get(0); Integer opDos = (Integer)salida.get(1);
+        Boolean condUnoN = (Boolean)salida.get(2); Boolean condDosN = (Boolean)salida.get(3);
+        Float opUnoF = (Float)salida.get(4); Float opDosF = (Float)salida.get(5);
+        Boolean condUnoF = (Boolean)salida.get(6); Boolean condDosF = (Boolean)salida.get(7);
+        
+        operaciones operacion = new operaciones();
+        
+        if (condUnoN && condDosN){ // ambos son enteros y están listo para realizar la operación
+            System.out.println("El resultado es: " + operacion.restaEntero(opUno, opDos));
+        }else if(condUnoN && condDosF){
+            float tempOpUno = (float)opUno;
+            System.out.println("El resultado es: " + operacion.restaFloat(tempOpUno, opDosF));
+        }else if(condUnoF && condDosN){
+            float tempOpDos = (float)opDos;
+            System.out.println("El resultado es: " + operacion.restaFloat(opUnoF, tempOpDos));
+        }else{ // ambos son flotantes
+            System.out.println("El resultado es: " + operacion.restaFloat(opUnoF, opDosF));
+        }
+    }
+    
+    ////////////////////////////////////////////
+    public void multiplicacion(){
+        System.out.println("\n[multiplicacion]");
+        LinkedList salida = processMain();
+        // Casting de variables que proceden del LinkedList
+        Integer opUno = (Integer)salida.get(0); Integer opDos = (Integer)salida.get(1);
+        Boolean condUnoN = (Boolean)salida.get(2); Boolean condDosN = (Boolean)salida.get(3);
+        Float opUnoF = (Float)salida.get(4); Float opDosF = (Float)salida.get(5);
+        Boolean condUnoF = (Boolean)salida.get(6); Boolean condDosF = (Boolean)salida.get(7);
+        
+        operaciones operacion = new operaciones();
+        
+        if (condUnoN && condDosN){ // ambos son enteros y están listo para realizar la operación
+            System.out.println("El resultado es: " + operacion.multiEntero(opUno, opDos));
+        }else if(condUnoN && condDosF){
+            float tempOpUno = (float)opUno;
+            System.out.println("El resultado es: " + operacion.multiFloat(tempOpUno, opDosF));
+        }else if(condUnoF && condDosN){
+            float tempOpDos = (float)opDos;
+            System.out.println("El resultado es: " + operacion.multiFloat(opUnoF, tempOpDos));
+        }else{ // ambos son flotantes
+            System.out.println("El resultado es: " + operacion.multiFloat(opUnoF, opDosF));
+        }
+    }
+    
+    ////////////////////////////////////////////
+    public void division(){
+        System.out.println("\n[division]");
+        LinkedList salida = processMain();
+        // Casting de variables que proceden del LinkedList
+        Integer opUno = (Integer)salida.get(0); Integer opDos = (Integer)salida.get(1);
+        Boolean condUnoN = (Boolean)salida.get(2); Boolean condDosN = (Boolean)salida.get(3);
+        Float opUnoF = (Float)salida.get(4); Float opDosF = (Float)salida.get(5);
+        Boolean condUnoF = (Boolean)salida.get(6); Boolean condDosF = (Boolean)salida.get(7);
+        
+        operaciones operacion = new operaciones();
+        
+        if (condUnoN && condDosN){ // ambos son enteros y están listo para realizar la operación
+            System.out.println("El resultado es: " + operacion.divisionEntero(opUno, opDos));
+        }else if(condUnoN && condDosF){
+            float tempOpUno = (float)opUno;
+            System.out.println("El resultado es: " + operacion.divisionFloat(tempOpUno, opDosF));
+        }else if(condUnoF && condDosN){
+            float tempOpDos = (float)opDos;
+            System.out.println("El resultado es: " + operacion.divisionFloat(opUnoF, tempOpDos));
+        }else{ // ambos son flotantes
+            System.out.println("El resultado es: " + operacion.divisionFloat(opUnoF, opDosF));
+        }
+    }
+
+    ////////////////////////////////////////////
+    public void modulo(){
+        System.out.println("\n[modulo]");
+        LinkedList salida = processMain();
+        // Casting de variables que proceden del LinkedList
+        Integer opUno = (Integer)salida.get(0); Integer opDos = (Integer)salida.get(1);
+        Boolean condUnoN = (Boolean)salida.get(2); Boolean condDosN = (Boolean)salida.get(3);
+        Float opUnoF = (Float)salida.get(4); Float opDosF = (Float)salida.get(5);
+        Boolean condUnoF = (Boolean)salida.get(6); Boolean condDosF = (Boolean)salida.get(7);
+        
+        operaciones operacion = new operaciones();
+        
+        if (condUnoN && condDosN){ // ambos son enteros y están listo para realizar la operación
+            System.out.println("El resultado es: " + operacion.moduloEntero(opUno, opDos));
+        }else if(condUnoN && condDosF){
+            float tempOpUno = (float)opUno;
+            System.out.println("El resultado es: " + operacion.moduloFloat(tempOpUno, opDosF));
+        }else if(condUnoF && condDosN){
+            float tempOpDos = (float)opDos;
+            System.out.println("El resultado es: " + operacion.moduloFloat(opUnoF, tempOpDos));
+        }else{ // ambos son flotantes
+            System.out.println("El resultado es: " + operacion.moduloFloat(opUnoF, opDosF));
+        }
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public LinkedList processSpecificTwoOps (){
+        LinkedList salida = new LinkedList();
+        
+        double opUno= 0; double opDos = 0;
+        
+        Scanner operandoUno = new Scanner(System.in);
+        Scanner operandoDos = new Scanner(System.in);
+        try{
+            System.out.print("Ingresa la base > ");
+            opUno = operandoUno.nextDouble();
+        }catch(Exception e){
+            System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero o flotante.\n");
+            processSpecificTwoOps();
+        }
+        try{
+            System.out.print("Ingresa el exponente > ");
+            opDos = operandoDos.nextDouble();
+        }catch(Exception e){
+            System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero o flotante.\n");
+            processSpecificTwoOps();
+        }
+        salida.add(opUno); salida.add(opDos);
+        return salida;
+    }
+    
+    public void potenciaG(){
+        System.out.println("\n[potencia_generica]");
+        LinkedList salida = processSpecificTwoOps();
+        // Casting
+        Double opUno = (Double)salida.get(0); Double opDos = (Double)salida.get(1);
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.potenciaGenerica(opUno, opDos));
+    }
+
+    public void raizG(){
+        System.out.println("\n[raiz_generica]");
+        LinkedList salida = processSpecificTwoOps();
+        // Casting
+        Double opUno = (Double)salida.get(0); Double opDos = (Double)salida.get(1);
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.raziGenerica(opUno, opDos));
+    }
+    
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public double processSpecificOneOp (){
+        double opUno= 0;
+        Scanner operandoUno = new Scanner(System.in);
+        try{
+            System.out.print("Ingresa la base > ");
+            opUno = operandoUno.nextDouble();
+        }catch(Exception e){
+            System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero o flotante.\n");
+            processSpecificOneOp();
+        }
+        return opUno;
+    }
+    
+    ////////////
+    public void potenciaCua(){
+        System.out.println("\n[potencia_cuadrada]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.potenciaCuadrada(opUno));
+    }
+    
+    ////////////
+    public void potenciaCub(){
+        System.out.println("\n[potencia_cubica]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.potenciaCubica(opUno));
+    }
+    
+    ////////////
+    public void valorAbs(){
+        System.out.println("\n[valor_absoluto]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.absoluto(opUno));
+    }
+        
+    ////////////
+    public void redondear(){
+        System.out.println("\n[redondeo]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.redondeo(opUno));
+    }
+    
+    ////////////
+    public void senoT (){
+        System.out.println("\n[seno]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.seno(opUno));
+    }
+    
+    ////////////
+    public void cosenoT (){
+        System.out.println("\n[coseno]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.coseno(opUno));
+    }
+    
+    ////////////
+    public void tangenteT (){
+        System.out.println("\n[tangente]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.tangente(opUno));
+    }
+    
+    ////////////
+    public void arcSenoT (){
+        System.out.println("\n[arcseno]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.senoInv(opUno));
+    }
+    
+    ////////////
+    public void arcCosenoT (){
+        System.out.println("\n[arccoseno]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.cosenoInv(opUno));
+    }
+    
+    ////////////
+    public void arcTangenteT (){
+        System.out.println("\n[arctangente]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.tangenteInv(opUno));
+    }
+    
+    ////////////
+    public void logaritmoNatural (){
+        System.out.println("\n[logaritmo_natual]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.logNatural(opUno));
+    }
+
+    ////////////
+    public void logaritmoBase10 (){
+        System.out.println("\n[logaritmo_base_10]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.logBase10(opUno));
+    }
+    
+    ////////////
+    public void raizCua (){
+        System.out.println("\n[logaritmo_base_10]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.raizCuadrada(opUno));
+    }
+
+    ////////////
+    public void raizCub (){
+        System.out.println("\n[logaritmo_base_10]");
+        double opUno = processSpecificOneOp();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.raizCubica(opUno));
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public int processSpecificOneOpInt (){
+        int opUno= 0;
+        Scanner operandoUno = new Scanner(System.in);
+        try{
+            System.out.print("Ingresa la base > ");
+            opUno = operandoUno.nextInt();
+        }catch(Exception e){
+            System.out.println("\n\t[error]: Se ha ingresado un operando incorrecto, debe ser un numero entero.\n");
+            processSpecificOneOp();
+        }
+        return opUno;
+    }
+
+    ////////////
+    public void factorial (){
+        System.out.println("\n[factorial]");
+        int opUno = processSpecificOneOpInt();
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.factorialRecursivo(opUno));
+    }
+    
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void constanteE (){
+        System.out.println("\n[constante_e]");
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.constE());
+    }
+    
+    ////////////
+    public void constantePI (){
+        System.out.println("\n[constante_pi]");
+        operaciones operacion = new operaciones();        
+        System.out.println("El resultado es: " + operacion.constPI());
+    }
+    
 }
